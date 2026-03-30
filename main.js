7f4ba9cab734052e79896b81c73850dbd83d4c06
chiragjain10 <jainservices2000@gmail.com>
2026-03-30 12:31:32 +0530

    Backup files and changes in main.js

--- main.js
+++ main.js
@@ -1,63 +1,10 @@
-// window.alert()
-// window.prompt()
-// window.confirm()
-// window.open()
+// let url = "https://jsonplaceholder.typicode.com/users";
 
-
-// window.open("https://google.com","","Mohit");
-
-// window.navigator.clipboard.write()
-
-// window.print()
-
-// window.History.toString()
-
-async function CopyElement(button) {
-    let parent = button.parentElement;
-    let p = parent.querySelector("p");
-    try {
-        await window.navigator.clipboard.writeText(p.innerText);
-        console.log("Content is successfully copied")
-    }
-    catch (error) {
-        console.log(error.message);
-    }
+const fetchApi = (url)=>{
+    fetch(url).then(v=>{
+        console.log(v);
+    })
 }
 
-// const pasteContent = async (button) => {
-//     let parent = button.parentElement;
-//     let p = parent.querySelector("p");
-//     try {
-//         p.innerText = await window.navigator.clipboard.readText();
-//     }
-//     catch (error) {
-//         console.log(error.message);
-//     }
-// }
 
-
-// Synchronous & Asynchronous
-
-
-// Reqeust -> handle -> optimize -> next
-
-
-
-// async
-// await
-
-
-// let a = 5;
-// console.log(a);
-// console.log(email)
-// console.log(a);
-
-// try{
-//     let a = 5;
-//     console.log(a);
-//     console.log(email);
-//     console.log(a);
-// }
-// catch(error){
-//     console.log(error.message)
-// }
+fetchApi("https://jsonplaceholder.typicode.com/users")
